3d0c6f158817bd16eca392bb8c0995f31261ca35
Patrick <accounts@pp83.de>
2020-09-16 14:44:47 +0200

    upload

---
+++ dadad.py
@@ -0,0 +1,8 @@
+a = "h3llo"
+
+b = a+"abs"
+c = "abs%s" % (a)
+
+print(b)
+print(c)
+print("hallo")
---
+++ path.py
@@ -0,0 +1,4 @@
+import os
+
+print(os.path.join(os.path.dirname(__file__),"PLZ.db"))
+print(__file__)
